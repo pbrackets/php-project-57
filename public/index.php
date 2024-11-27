ee8b27d4b4cbc39eb06e968607a927308bfd4543
aelita <ai2work@yandex.ru>
2024-11-27 15:43:52 +0500

    laracasts/flash

--- public/index.php
+++ public/index.php
@@ -15,3 +15,32 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+//подключение БД
+$envDbUrl = getenv('DATABASE_URL');
+if ($envDbUrl === false) {
+    die('Отсутствует переменная окружения DATABASE_URL');
+}
+$databaseUrl = parse_url($envDbUrl);
+
+//$databaseUrl = parse_url($_ENV['DATABASE_URL']);
+$username = $databaseUrl['user'];             // janedoe
+$password = $databaseUrl['pass'];             // mypassword
+$host     = $databaseUrl['host'];             // localhost
+$port     = $databaseUrl['port'];             // 5432
+$dbName   = ltrim($databaseUrl['path'], '/'); // mydb
+$db_table = 'users';                           // Имя Таблицы БД
+
+//формируем dsn для подключения
+$dsn = "pgsql:host=".$host.";port=".$port.";dbname=".$dbName;
+//PDO подключение к базе данных
+$db = new PDO($dsn, $username, $password);
+
+// public function store()
+// {
+//     flash('Welcome Aboard!')->success();
+//
+//     return home();
+// }
+
+
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
