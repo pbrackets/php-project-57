1fd8ce5f4ff0d0f576e4bcb904a6f97afa472b5f
aelita <ai2work@yandex.ru>
2025-03-21 17:25:40 +0500

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,61 +2,14 @@
 @section('content')
 
     <div class="grid col-span-full">
-{{--        <h1 class="max-w-2xl mb-4 text-4xl leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">--}}
-{{--            {{ __('layout.task_statuses_header') }}--}}
-{{--        </h1>--}}
-{{--        @auth()--}}
-{{--            <div>--}}
-{{--                @csrf--}}
-{{--                <a href="{{ route('task_statuses.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">--}}
-{{--                    {{ __('layout.create_button_task_statuses') }}           </a>--}}
-{{--            </div>--}}
-{{--        @endauth--}}
-{{--        <table class="mt-4">--}}
-{{--            <thead class="border-b-2 border-solid border-black text-left" style="text-align: left">--}}
-{{--            <tr>--}}
-{{--                <th>{{ __('layout.table_id') }}</th>--}}
-{{--                <th>{{ __('layout.table_name') }}</th>--}}
-{{--                <th>{{ __('layout.table_date_of_creation') }}</th>--}}
-{{--                @auth()--}}
-{{--                    <th>{{ __('layout.table_actions') }}</th>--}}
-{{--                @endauth--}}
-{{--            </tr>--}}
-{{--            </thead>--}}
-{{--            <tbody>--}}
-{{--            @foreach($taskStatuses as $taskStatus)--}}
-{{--                <tr class="border-b border-dashed text-left">--}}
-{{--                    <td>{{ $taskStatus->id }}</td>--}}
-{{--                    <td>{{ $taskStatus->name }}</td>--}}
-{{--                    <td>{{ $taskStatus->created_at }}</td>--}}
-{{--                    @auth()--}}
-{{--                        <td>--}}
-{{--                            <a--}}
-{{--                                class="text-red-600 hover:text-red-900"--}}
-{{--                                rel="nofollow"--}}
-{{--                                data-method="delete"--}}
-{{--                                data-confirm="{{ __('layout.table_delete_question') }}"--}}
-{{--                                href="{{ route('task_statuses.destroy', $taskStatus) }}"--}}
-{{--                            >--}}
-{{--                                {{ __('layout.table_delete') }}--}}
-{{--                            </a>--}}
-{{--                            <a class="text-blue-600 hover:text-blue-900"--}}
-{{--                               href="{{ route("task_statuses.edit", $taskStatus) }}"--}}
-{{--                            >--}}
-{{--                                {{ __('layout.table_edit') }}--}}
-{{--                            </a>--}}
-{{--                        </td>--}}
-{{--                    @endauth--}}
-{{--                </tr>--}}
-{{--            @endforeach--}}
-{{--            </tbody>--}}
-{{--        </table>--}}
-{{--    </div>--}}
+        <h1 class="mb-6 text-white text-4xl">
+            Привет от Хекслета!
+        </h1>
+        <p class="mb-6 text-white text-4xl">
+            Это простой менеджер задач на Laravel
+        </p>
+        <button><a href="https://hexlet.io" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" target="_blank">
+                Нажми меня            </a></button>
+    </div>
 
-
-
-
-{{--    @auth()--}}
-{{--        <div class="mt-4 grid col-span-full">{{ $taskStatuses->links() }}</div>--}}
-{{--    @endauth--}}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
 </div>
 
 <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+    <div class="grid max-w-screen-xl px-4 py-10 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
 {{--        <div class="mt-4 grid col-span-full">@include('flash::message')</div>--}}
         @yield('content')
     </div>
